792d3ef54406e1b96d76a9402e92c754ff49fe9d
naufal aufa <muhammadnaufalaufarifqi@gmail.com>
2024-05-13 16:55:43 +0700

    seven commit in senin 13/2024

--- src/components/DuaTipe.jsx
+++ src/components/DuaTipe.jsx
@@ -1,8 +1,29 @@
+import { Image } from "antd";
+import TwoTypeImage from "../assets/image/twoTypeOne.jpg";
+import { FaYoutube } from "react-icons/fa";
+
 const DuaTipe = () => {
   return (
-    <section className="min-h-screen grid place-content-center place-items-center">
-      <div>
-        <h1 className="text-center font-bold text-2xl">Dua Tipe</h1>
+    <section className="min-h-screen grid place-content-center place-items-center my-32">
+      <div className="w-screen max-w-[95vw] m-auto">
+        <h1 className="text-center font-bold text-2xl my-3">Dua Tipe</h1>
+        <Image className="rounded-lg" src={TwoTypeImage} alt={TwoTypeImage} />
+        <p className="text-[10px] w-screen max-w-[90%] m-auto text-center mt-3">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae
+          necessitatibus rem facere minus cum tenetur fuga, veniam quo fugit
+          sapiente magnam quisquam voluptatibus sint, quas accusantium, atque
+          harum iure natus?
+        </p>
+        <a
+          href=""
+          target="_blank"
+          className="flex items-center hover:bg-black hover:border duration-75 w-[150px] p-2 rounded-lg justify-center gap-3 m-auto my-5 bg-red-600 text-white"
+        >
+          <p>Youtube</p>
+          <span>
+            <FaYoutube />
+          </span>
+        </a>
       </div>
     </section>
   );
--- src/components/SiapaBatchSE.jsx
+++ src/components/SiapaBatchSE.jsx
@@ -1,10 +1,11 @@
 import { Image } from "antd";
 import SiapaImage from "../assets/image/siapaImageOne.jpg";
+import { FaYoutube } from "react-icons/fa";
 
 const SiapaBatchSE = () => {
   return (
     <>
-      <section className="grid min-h-screen place-content-center place-items-center ">
+      <section className="grid min-h-screen place-content-center my-32 place-items-center ">
         <h1 className="my-5 font-bold text-2xl">Siapa</h1>
         <div className="w-screen max-w-[95vw] m-auto">
           <Image className="rounded-lg" src={SiapaImage} alt={SiapaImage} />
@@ -14,6 +15,16 @@ const SiapaBatchSE = () => {
             molestias voluptate provident reprehenderit minima, labore aliquam
             aliquid optio laboriosam impedit voluptatum. lorem
           </p>
+          <a
+            href=""
+            target="_blank"
+            className="flex items-center w-[150px] p-2 rounded-lg justify-center gap-3 m-auto my-5 bg-red-600 text-white hover:bg-black hover:border duration-75 hover:text-white"
+          >
+            <p>Youtube</p>
+            <span>
+              <FaYoutube />
+            </span>
+          </a>
         </div>
       </section>
     </>
